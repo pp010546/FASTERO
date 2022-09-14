2cd229486aae0184719f6aa457a0e58e2cca98c4
Philip <philip0105@gmail.com>
2022-09-15 02:51:18 +0800

    add all

---
+++ src/main/java/com/fastero/bean/LoginBean.java
@@ -0,0 +1,28 @@
+package com.fastero.bean;
+
+public class LoginBean {
+
+	private String account;
+	private String password;
+
+	public LoginBean() {
+
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
