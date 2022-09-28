82908335816ef1d9d2f4852cae52cc7172825c9d
Pei <m34672@gmail.com>
2022-09-28 09:38:48 +0800

    update SQL

--- src/main/java/com/fastero/dao/sql/OrderMasterSQL.java
+++ src/main/java/com/fastero/dao/sql/OrderMasterSQL.java
@@ -2,7 +2,6 @@ package com.fastero.dao.sql;
 
 public class OrderMasterSQL {
 
-<<<<<<< HEAD
 	// order_id, user_id, store_id, order_status, order_amount
 	// order_time, update_time, order_remark
 	public static final String GET_ALL = "SELECT * FROM `FASTERO`.`OrderMaster`";
@@ -16,12 +15,5 @@ public class OrderMasterSQL {
 	// `EXHIBITION_IMG` = ? WHERE (`EXHIBITION_ID` = ?)";
 
 	public static final String DELETE = "DELETE FROM `FASTERO`.`OrderMaster` WHERE (`order_id` = ?);";
-=======
-	public static final String GET_ALL = "SELECT * FROM FASTERO.OrderMaster";
-<<<<<<< HEAD
->>>>>>> 74306723e816e3442be7337ef15742d774d15e00
 
-=======
-	public static final String GET_BY_ID = "SELECT * FROM FASTERO.OrderMaster where order_id = ?";
->>>>>>> Philip
 }
